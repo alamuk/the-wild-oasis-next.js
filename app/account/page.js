48f388265c822cfbd0c8c038b220c3c461e2c755
ALAM <alam.2008@live.co.uk>
2024-09-24 19:52:16 +0100

    components and pages setup

--- app/account/page.js
+++ app/account/page.js
@@ -1,3 +1,11 @@
+export const metadata = {
+  title: 'Accounts',
+};
+
 export default function Page() {
-  return <div>Account page</div>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome Shah, to Your Accounts!
+    </h2>
+  );
 }
